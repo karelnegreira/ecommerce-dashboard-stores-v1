981201d68950a0913c20b04d7226a2df4b90b73a
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-03-04 21:43:28 +1200

    first commit about stores with info, product list, gallery and other components already impl

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import getProducts from "@/actions/get-products";
 import ProductList from "@/components/products-list";
 import Container from "@/components/ui/container";
 import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 
 
 interface ProductPageProps {
@@ -28,8 +29,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({params}) => {
                 <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
                     <Gallery images={product.images}/>
                     <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                    {/* Info */}
-                    Info
+                    <Info data={product} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              />
                     </div>
                 </div>
                 <hr className="my-10"/>
---
+++ components/info.tsx
@@ -0,0 +1,15 @@
+import { Product } from "@/types"
+
+interface InfoProps {
+    data: Product;
+}
+
+const Info: React.FC<InfoProps> = ({data}) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Info
