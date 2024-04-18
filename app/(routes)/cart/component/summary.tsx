88bdc8d3c0ca0dafe502f6c72849ca2deb703ca7
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-04-18 14:49:55 +1200

    summary.tsx created added static total and checkout button

--- app/(routes)/cart/component/summary.tsx
+++ app/(routes)/cart/component/summary.tsx
@@ -11,8 +11,17 @@ import useCart from "@/hooks/use-toast";
 
 const Summary = () => {
     return (
-        <div>
-            Summary
+        <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+            <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
+            <div className="mt-6 space-y-4">
+                <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+                    <div className="text-base font-medium text-gray-900">
+                        Order total
+                    </div>
+                    <Currency value={45}/>
+                </div>
+            </div>
+            <Button className="w-full mt-6">Checkout</Button>
         </div>
     )
 }
